611c52190e202967ab78ca6715904830c404c99b
labake <labakelemboye@gmail.com>
2016-11-30 17:47:30 +0100

    Query API

--- API/API.js
+++ API/API.js
@@ -1,13 +1,33 @@
  
 var request = require ("request")
-request({
-  url: 'http://api.geonames.org/citiesJSON',
-  qs: {username: 'happygirl01', formatted: true, north: 44.1, south: -9.9, east: -22, west: 55.2, style: "full"}, 
+var readline = require('readline')
+var rl = readline.createInterface({
+ input: process.stdin,
+ output: process.stdout
+});
+
+console.log('enter your username')
+
+rl.question('username: ', (answer) => {
+ // TODO: Log the answer in a database
+ request({
+  url: 'http://api.geonames.org/citiesJSON/', //URL to hit
+  qs: {username: answer, formatted: true, north: 44.1, south: -9.9, east: -22, west: 55.2, style: "full", maxRows: 3 }, //Query string data
   method: 'POST',
 }, function(error, response, body){
   if(error) {
       console.log(error);
   } else {
-      console.log(response.statusCode, body);
+  	var myResponse = JSON.parse(body);
+    var cities = myResponse['geonames'];
+    var name = process.argv[2]
+    for (var i=0;i<cities.length;i++) {
+    	if (cities[i].name == name) {
+    		console.log(cities[i])
+    	}
+    }
   }
+});
+
+ rl.close();
 });
